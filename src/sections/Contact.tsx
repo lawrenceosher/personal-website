513864e40c26e3158e506e85f81cb7485b81079f
Lawrence Osher <lao2205@gmail.com>
2025-05-18 19:26:51 -0400

    finished contact section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,9 +1,49 @@
+import { MdOutlineEmail } from "react-icons/md";
+import { CiLinkedin } from "react-icons/ci";
+import { FiGithub } from "react-icons/fi";
+import { FaRegCopyright } from "react-icons/fa";
+
+import "../styles/Contact.css";
+
 export default function Contact() {
   return (
-    <section className="d-flex flex-column left-0 right-0 p-3">
+    <div className="d-flex flex-column left-0 right-0 p-3">
       <h2 id="contact" className="display-5 fw-bold section-header text-end">
         Contact
       </h2>
-    </section>
+
+      <div className="d-flex flex-column">
+        <a
+          id="contact-link"
+          className="d-flex align-items-center fs-1 text-decoration-none text-white m-0 p-0"
+          href="mailto:osher.lawrence@gmail.com"
+        >
+          <MdOutlineEmail className="me-2 mt-1" />{" "}
+          <span>osher.lawrence@gmail.com</span>
+        </a>
+
+        <a
+          id="contact-link"
+          className="d-flex align-items-center fs-1 text-decoration-none text-white m-0 p-0"
+          href="https://www.linkedin.com/in/losher/"
+          target="_blank"
+        >
+          <CiLinkedin className="me-2 mt-1" /> <span>losher</span>
+        </a>
+
+        <a
+          id="contact-link"
+          className="d-flex align-items-center fs-1 text-decoration-none text-white m-0 p-0"
+          href="https://github.com/lawrenceosher"
+          target="_blank"
+        >
+          <FiGithub className="me-2 mt-1" /> <span>lawrenceosher</span>
+        </a>
+      </div>
+
+      <p className="mt-5 text-center">
+        <FaRegCopyright className="me-1 mb-1" /> Lawrence Osher 2025
+      </p>
+    </div>
   );
 }
