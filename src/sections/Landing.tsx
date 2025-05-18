38dd7f0f8d7428901e2f3546075692505bd33794
Lawrence Osher <lao2205@gmail.com>
2025-05-18 02:46:14 -0400

    centering landing

--- src/sections/Landing.tsx
+++ src/sections/Landing.tsx
@@ -2,7 +2,7 @@ import "../App.css";
 
 export default function Landing() {
   return (
-    <section className="d-flex flex-column">
+    <section id="landing" className="d-flex flex-column align-items-center justify-content-center">
       <h1 id="name" className="fw-bold m-2 display-3">
         Lawrence Osher
       </h1>
