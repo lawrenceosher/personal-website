e3b7bf9f3ea225d8a7407a1370cf7f6b6fe3fa78
Lawrence Osher <lao2205@gmail.com>
2025-05-18 02:12:34 -0400

    Moving the header to app.tsx and adding more experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,54 @@
 import "./App.css";
-import Header from "./sections/Header";
+import Landing from "./sections/Landing";
 import About from "./sections/About";
 import Experience from "./sections/Experience";
 import Projects from "./sections/Projects";
 import Contact from "./sections/Contact";
+import { Navbar, Nav } from "react-bootstrap";
+import { LiaProjectDiagramSolid } from "react-icons/lia";
+import { LuIdCard } from "react-icons/lu";
+import { RiBriefcase2Line, RiContactsLine } from "react-icons/ri";
 
 function App() {
   return (
     <>
-      <Header />
+      <Navbar
+        id="header"
+        className="flex-fill justify-content-end"
+        sticky="top"
+      >
+        <Nav className="fs-3 me-2">
+          <Nav.Link
+            href="#about"
+            className="header-item me-2 d-flex align-items-center"
+          >
+            <LuIdCard className="me-2" />
+            About
+          </Nav.Link>
+          <Nav.Link
+            href="#experience"
+            className="header-item me-2 d-flex align-items-center"
+          >
+            <RiBriefcase2Line className="me-2" />
+            Experience
+          </Nav.Link>
+          <Nav.Link
+            href="#projects"
+            className="header-item me-2 d-flex align-items-center"
+          >
+            <LiaProjectDiagramSolid className="me-2" />
+            Projects
+          </Nav.Link>
+          <Nav.Link
+            href="#contact"
+            className="header-item d-flex align-items-center"
+          >
+            <RiContactsLine className="me-2" />
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+      <Landing />
       <About />
       <Experience />
       <Projects />
--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -8,14 +8,14 @@ export default function Experience() {
         Experience
       </h2>
 
-      <Tabs defaultActiveKey="Chewy" className="mb-3">
+      <Tabs id="experience" defaultActiveKey="Chewy" className="mb-3">
         <Tab eventKey="Chewy" title="Chewy">
           <div className="d-flex align-items-center mb-3">
             <h1 className="flex-fill">Software Engineer Co-op</h1>
             <div className="fs-3">
               <span>June 2024 - December 2024</span>
               <span className="mx-2"> | </span>
-              <span className="">Boston, MA</span>
+              <span>Boston, MA</span>
             </div>
           </div>
 
@@ -59,10 +59,74 @@ export default function Experience() {
           </ul>
         </Tab>
         <Tab eventKey="HealthStream" title="HealthStream">
-          Tab content for Profile
+          <div className="d-flex align-items-center mb-3">
+            <h1 className="flex-fill">Software Developer Intern</h1>
+            <div className="fs-3">
+              <span>September 2023 - December 2023</span>
+              <span className="mx-2"> | </span>
+              <span>Remote</span>
+            </div>
+          </div>
+
+          <p>
+            I was part of the Cloud CME team working on the 'Activity Editor' so
+            any institutions using the LMS could manage any activity or event
+            they set up for their medical professionals.
+          </p>
+
+          <ul>
+            <li>
+              Overhauled 10 frontend pages using React.js to update CloudCME's
+              learning product for a more “modern” feel for medical
+              professionals
+            </li>
+            <li>
+              Learned about the workflow of making a frontend page from mock-up
+              to page layout to adding functionality to adding styling
+            </li>
+            <li>
+              Gained experience with key React patterns such as state management
+              for conditional rendering, outputting lists, and working with user
+              input through forms
+            </li>
+            <li>Styled all pages with Tailwind CSS framework</li>
+            <li>
+              Implemented 15 CRUD endpoints with the Entity Framework for users
+              to dynamically retrieve and update data
+            </li>
+            <li>
+              Participated in Agile ceremonies such as sprint reviews and
+              retrospectives for full-stack development and UI/UX design
+            </li>
+          </ul>
         </Tab>
         <Tab eventKey="Generate" title="Generate">
-          Tab content for Contact
+          <div className="d-flex align-items-center mb-3">
+            <h1 className="flex-fill">Software Engineer</h1>
+            <div className="fs-3">
+              <span>January 2023 - April 2023</span>
+              <span className="mx-2"> | </span>
+              <span>Boston, MA</span>
+            </div>
+          </div>
+
+          <p>
+            Generate is Northeastern's Student-Led Product Development Studio,
+            and I was part of the 'FindHer' team.
+          </p>
+
+          <ul>
+            <li>
+              Participated in code reviews and team meetings for 'FindHer' web
+              application that provides women in India with a platform for job
+              searches
+            </li>
+            <li>Utilized Python for backend development of endpoints</li>
+            <li>
+              Developed React components, such as a drag and drop ranking system
+              and a file uploader, for the frontend of a REST API
+            </li>
+          </ul>
         </Tab>
       </Tabs>
     </div>
